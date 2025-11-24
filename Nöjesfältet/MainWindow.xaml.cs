449f5bb8aa15299a70d849e944525914f1cc3ff3
josefinnorr <josno2505@student.miun.se>
2025-11-24 09:29:13 +0100

    Nöjesfältet - Del 1

---
+++ Nöjesfältet/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nöjesfältet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int _length;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            _length = int.Parse(txtLength.Text);
+
+            if(_length < 140)
+            {
+                MessageBox.Show("Tyvärr, du är inte tillräckligt lång för att åka.");
+            }
+            else
+            {
+                MessageBox.Show("Du är tillräckligt lång för att åka!");
+            }
+        }
+    }
+}
